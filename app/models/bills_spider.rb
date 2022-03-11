60e3c79be039a80097ab545456c3c8d8915c9605
VictorHAlbu <vh12albuquerque@gmail.com>
2022-03-11 13:03:04 -0300

    Web Scraping com kimurai

--- app/models/bills_spider.rb
+++ app/models/bills_spider.rb
@@ -8,14 +8,18 @@ class BillsSpider < Kimurai::Base
   end
 
   def parse(response, url:, data: {})
-    response.xpath("//table[@class='table table-striped table-bordered']").each do |bill|
-      debugger
+    response.xpath("//div[@class='container-detail clearfix']//tbody").each do |bill|
       item = {}
-
-      item[:menu] = bill.css('div.dont-break-out')&.text&.squish
-      item[:presentation] = bill.css('strong.dont-break-out')&.text&.squish
-
       
+      item[:menu] = bill.css('div.dont-break-out')[0]&.text&.squish
+      item[:presentation] =  bill.css('strong')[2]&.next.text&.squish
+      item[:author] =  bill.css('strong')[3]&.next.text&.squish
+      item[:current_location] =  bill.css('strong')[4]&.next.text&.squish
+      item[:status] =  bill.css('strong')[5]&.next.text&.squish
+      item[:end_date_deadline] =  bill.css('strong')[6]&.next.text&.squish
+      item[:last_processing_date] =  bill.css('strong')[7]&.next.text&.squish
+      item[:last_action] =  bill.css('strong')[8]&.next.text&.squish
+
 
       Bill.where(item).first_or_create
     end
