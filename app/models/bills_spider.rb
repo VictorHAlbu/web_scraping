78c1d3fcc31ea8618c6cb7bcb245871d8c72e00e
VictorHAlbu <vh12albuquerque@gmail.com>
2022-03-12 13:03:27 -0300

    refatoring

--- app/models/bills_spider.rb
+++ app/models/bills_spider.rb
@@ -9,19 +9,16 @@ class BillsSpider < Kimurai::Base
 
   def parse(response, url:, data: {})
     response.xpath("//table[@class='table table-striped table-bordered']").each do |bill|
-      item = {}
-      
-      item[:menu] = bill.css('div.dont-break-out')[0]&.text&.squish
-      item[:presentation] =  bill.css('strong')[2]&.next.text&.squish
-      item[:author] =  bill.css('strong')[3]&.next.text&.squish
-      item[:current_location] =  bill.css('strong')[4]&.next.text&.squish
-      item[:status] =  bill.css('strong')[5]&.next.text&.squish
-      item[:end_date_deadline] =  bill.css('strong')[6]&.next.text&.squish&.to_date
-      item[:last_processing_date] =  bill.css('strong')[7]&.next.text&.squish&.to_date
-      item[:last_action] =  bill.css('strong')[8]&.next.text&.squish
-
-
-      Bill.where(item).first_or_create
+      item = Bill.new
+        item[:menu] = bill.css('div.dont-break-out')[0]&.text&.squish
+        item[:presentation] =  bill.css('strong')[2]&.next.text&.squish
+        item[:author] =  bill.css('strong')[3]&.next.text&.squish
+        item[:current_location] =  bill.css('strong')[4]&.next.text&.squish
+        item[:status] =  bill.css('strong')[5]&.next.text&.squish
+        item[:end_date_deadline] =  bill.css('strong')[6]&.next.text&.squish&.to_date
+        item[:last_processing_date] =  bill.css('strong')[7]&.next.text&.squish&.to_date
+        item[:last_action] =  bill.css('strong')[8]&.next.text&.squish
+      item.save!
     end
   end
 end
