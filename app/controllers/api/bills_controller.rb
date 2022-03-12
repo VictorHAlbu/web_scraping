5ddbd2b19d0157a325909348543b7b2fb2933f29
VictorHAlbu <vh12albuquerque@gmail.com>
2022-03-12 12:29:35 -0300

    filtro por author

--- app/controllers/api/bills_controller.rb
+++ app/controllers/api/bills_controller.rb
@@ -2,11 +2,16 @@ module Api
   class BillsController < ApplicationController   
 
     def index
-      bills = Bill.all
-      render json: {status: 'SUCCESS', message:'Bills carregados', data:bills},status: :ok
+      @bills = Bill.all
+      filter_by_author if params[:author]
+      render json: {status: 'SUCCESS', message:'Bills carregados', data:@bills},status: :ok
     end
 
     private
+
+    def filter_by_author
+      @bills = @bills.where(author: params[:author])
+    end
 
     def bill_params
       params.require(:bill).permit(:menu, :presentation, :author, :current_location, :status, :end_date_deadline, :last_processing_date, :last_action)
